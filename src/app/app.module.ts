80602f3ec30f3453b82fdd7c45f962e1eed911ec
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2022-05-20 16:40:45 -0300

    Serviço de mensagem implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,8 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
+import { timeout } from 'rxjs';
 
 
 @NgModule({
@@ -64,6 +66,11 @@ import { LoginComponent } from './components/login/login.component';
     MatCardModule,
     FormsModule,
     ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credencial } from 'src/app/models/Credencial';
 
 @Component({
@@ -16,11 +17,16 @@ export class LoginComponent implements OnInit {
 
   email = new FormControl(null,Validators.email);
   senha = new FormControl(null,Validators.minLength(3));
-  constructor() { }
+  constructor(private toast: ToastrService) { }
 
   ngOnInit(): void {
   }
 
+  logar(){
+    this.toast.error('Usuário não encontrado!','Login')
+    this.cred.senha = '';
+    this.cred.email = '';
+  }
   desabilitarCampo(): boolean {
       if(!this.email.valid || !this.senha.valid){
         return true;
