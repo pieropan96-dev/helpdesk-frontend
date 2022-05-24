6b47e44cab4c7917aceb490cf1274fe8efac4301
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2022-05-24 09:38:18 -0300

    Autentificação de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credencial } from 'src/app/models/Credencial';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -15,22 +17,23 @@ export class LoginComponent implements OnInit {
     senha: ''
   }
 
-  email = new FormControl(null,Validators.email);
-  senha = new FormControl(null,Validators.minLength(3));
-  constructor(private toast: ToastrService) { }
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+  constructor(private toast: ToastrService, private service: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  logar(){
-    this.toast.error('Usuário não encontrado!','Login')
-    this.cred.senha = '';
-    this.cred.email = '';
+  logar() {
+    this.service.authenticate(this.cred).subscribe(resposta => {
+      this.service.successfulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate([''])
+    }, () => {
+      this.toast.error('Usuário e/ou senha inválidos');
+    })
   }
+
   desabilitarCampo(): boolean {
-      if(!this.email.valid || !this.senha.valid){
-        return true;
-      }
-      return false;
+    return !this.email.valid || !this.senha.valid
   }
 }
---
+++ src/app/config/api.config.ts
@@ -0,0 +1,3 @@
+export const API_CONFIG = {
+    baseUrl: 'https://helpdesk-backend-pieropan.herokuapp.com'
+}
