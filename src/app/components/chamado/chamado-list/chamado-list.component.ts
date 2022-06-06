ec56a15ea7e8cf1aa555a65f9758e14d728283ed
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2022-06-06 15:10:01 -0300

    HTML e css para component chamado-list

---
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chamado } from 'src/app/models/Chamado';
+
+@Component({
+  selector: 'app-chamado-list',
+  templateUrl: './chamado-list.component.html',
+  styleUrls: ['./chamado-list.component.css']
+})
+export class ChamadoListComponent implements OnInit {
+
+  ELEMENT_DATA: Chamado[] = []
+  FILTERED_DATA: Chamado[] = []
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  orderByStatus(status: any): void{
+    let list: Chamado[] = []
+    this.ELEMENT_DATA.forEach(element => {
+      if(element.status == status)
+        list.push(element)
+    });
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Chamado>(list);
+    this.dataSource.paginator = this.paginator;
+  }
+
+}
