faef882e5b9696d420b53031ca67ba31e35d1682
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2022-06-06 17:10:07 -0300

    Filtro e  método find all para chamados

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Chamado } from 'src/app/models/Chamado';
+import { ChamadoService } from 'src/app/services/chamado.service';
 
 @Component({
   selector: 'app-chamado-list',
@@ -12,13 +13,18 @@ export class ChamadoListComponent implements OnInit {
 
   ELEMENT_DATA: Chamado[] = []
   FILTERED_DATA: Chamado[] = []
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private service: ChamadoService
+  ) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
   applyFilter(event: Event) {
@@ -26,15 +32,43 @@ export class ChamadoListComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  orderByStatus(status: any): void{
+  orderByStatus(status: any): void {
     let list: Chamado[] = []
     this.ELEMENT_DATA.forEach(element => {
-      if(element.status == status)
+      if (element.status == status)
         list.push(element)
     });
     this.FILTERED_DATA = list;
     this.dataSource = new MatTableDataSource<Chamado>(list);
     this.dataSource.paginator = this.paginator;
+  }
+
+  findAll(): void {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Chamado>(resposta);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  retornaStatus(status: any): string {
+    if (status == '0') {
+      return 'ABERTO'
+    } else if (status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == '0') {
+      return 'BAIXA'
+    } else if (prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
   }
 
 }
