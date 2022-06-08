d126484d6e628d73c9afea3e06d326de3cfa1bd0
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2022-06-08 15:44:14 -0300

    Criação formulário chamado e validações

---
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Chamado } from 'src/app/models/Chamado';
+import { Cliente } from 'src/app/models/Cliente';
+import { Tecnico } from 'src/app/models/Tecnico';
+
+@Component({
+  selector: 'app-chamado-create',
+  templateUrl: './chamado-create.component.html',
+  styleUrls: ['./chamado-create.component.css']
+})
+export class ChamadoCreateComponent implements OnInit {
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes: '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
+
+  clientes: Cliente[] = []
+  tecnicos: Tecnico[] = []
+
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status:     FormControl = new FormControl(null, [Validators.required]);
+  titulo:     FormControl = new FormControl(null, [Validators.required]);
+  observacoes:FormControl = new FormControl(null, [Validators.required]);
+  tecnico:    FormControl = new FormControl(null, [Validators.required]);
+  cliente:    FormControl = new FormControl(null, [Validators.required]);
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid && this.status.valid && this.titulo.valid 
+       && this.observacoes.valid && this.tecnico.valid && this.cliente.valid
+  }
+
+}
