ef83e7db7b474a360d40838b8c6cfabc53681e38
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2022-06-10 15:59:41 -0300

    Ajustes no CSS

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -13,12 +13,12 @@ export class NavComponent implements OnInit {
   constructor(private router: Router, private authService: AuthService, private toast : ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['tecnicos'])
+    this.router.navigate(['home'])
   }
   
   logout(){
     this.router.navigate(['login']);
     this.authService.logout();
-    this.toast.info('Logout realizado!', 'Logout', {timeOut: 7000})
+    this.toast.info('Logout realizado!', 'Logout')
   }
 }
